a31dc87e404eafd0e76f1951b7e6e90d74b9ae1a
Joe Buhlig <joe@joebuhlig.com>
2016-10-24 09:33:12 -0500

    Add favicon

--- header.php
+++ header.php
@@ -17,6 +17,15 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
+
+<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/buffalo-covenant-theme/images/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/wp-content/themes/buffalo-covenant-theme/images/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/wp-content/themes/buffalo-covenant-theme/images/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/wp-content/themes/buffalo-covenant-theme/images/manifest.json">
+<link rel="mask-icon" href="/wp-content/themes/buffalo-covenant-theme/images/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/wp-content/themes/buffalo-covenant-theme/images/favicon.ico">
+<meta name="msapplication-config" content="/wp-content/themes/buffalo-covenant-theme/images/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
 <script>
   (function(d) {
     var config = {
