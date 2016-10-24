babf11990fec505e558f35fe0f31f7bb0c7c9e7b
Joe Buhlig <joe@joebuhlig.com>
2016-10-24 09:53:54 -0500

    Single sermon cleanup

--- single-sermon.php
+++ single-sermon.php
@@ -36,7 +36,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="articles">
 			<article>
 				<div class="sermon-archive-link"><a href="/sermons/">← Back to Archive</a></div>
 				<div class="sermon-thumbnail">
@@ -81,7 +80,6 @@ get_header(); ?>
 				<div><a href="https://geo.itunes.apple.com/us/podcast/buffalo-covenant-church-sermon/id1041139383?mt=2" style="display:inline-block;overflow:hidden;background:no-repeat;width: 100px;height: 40px;margin-right: 10px;"><img src="https://linkmaker.itunes.apple.com/images/badges/en-us/badge_itunes-lrg.svg"></a><a href="http://www.subscribeonandroid.com/buffalocov.libsyn.com/rss" style="display:inline-block;overflow:hidden;width: 108px;height: 41px;margin-right: 10px;"><img src="/wp-content/uploads/2016/10/sub-android.png"></a><a href="http://buffalocov.libsyn.com/rss" style="display:inline-block;overflow:hidden;width: 100px;height: 40px;"><img src="/wp-content/uploads/2016/10/rss@3x-82b6c168c05c2371ea3d094139a343dc.png"></a></div>
 				<?php the_post_navigation(); ?>
 			</article>
-			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
