14fcc472c8e66726c302ffdd4500d4e49c6a6c8e
Joe Buhlig <joe@joebuhlig.com>
2016-12-08 11:47:12 -0600

    Remove bio when nonexistent

--- template-parts/content-staff.php
+++ template-parts/content-staff.php
@@ -1,4 +1,4 @@
-<div class="staff-member" data-member-id="<?php echo get_the_ID() ?>">
+<div class="staff-member<? if ("" != trim(get_the_content())) : ?> has-bio pointer<? endif; ?>" data-member-id="<?php echo get_the_ID() ?>">
 	<div class="staff-member-pic-wrapper">
 		<div class="staff-member-pic">
 			<?php echo get_the_post_thumbnail() ?>
@@ -8,5 +8,7 @@
 			<div class="staff-member-title"><?php echo get_post_meta( get_the_ID(), "staff_title", true ) ?></div>
 		</div>
 	</div>
+	<? if ("" != trim(get_the_content())) : ?>
 	<div class="staff-member-bio"><?php the_content() ?></div>
+	<? endif; ?>
 </div>
