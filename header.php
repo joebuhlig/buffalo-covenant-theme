abeecc94ab40294648c0746294de707706f7a002
Joe Buhlig <joe@joebuhlig.com>
2016-12-19 14:14:13 -0600

    Undo header img selection

--- header.php
+++ header.php
@@ -98,10 +98,10 @@ global $page_title;
 				</div>
 			<?php else : ?>
 				<div class="poster page">
-					<?php if (wp_is_mobile() && get_post_meta( $post->ID, 'mobile_header', true)) : ?>
-						<img class="mobile-header" src="<?php echo esc_attr( get_post_meta( $post->ID, 'mobile_header', true) ) ?>" alt="">
-					<?php elseif ($show_default_header || !has_post_thumbnail()) : ?>
+					<?php if (($show_default_header || !has_post_thumbnail()) && !wp_is_mobile()) : ?>
 						<img src="<?php echo get_theme_mod('default_page_header') ?>" alt="">
+					<?php elseif (wp_is_mobile() && get_post_meta( $post->ID, 'mobile_header', true)) : ?>
+						<img class="mobile-header" src="<?php echo esc_attr( get_post_meta( $post->ID, 'mobile_header', true) ) ?>" alt="">
 					<?php else :
 						echo get_the_post_thumbnail();
 			        endif; ?>
